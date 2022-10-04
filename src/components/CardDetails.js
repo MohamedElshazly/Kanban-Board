ca2c907a88f6853d28d799c980934fc04cfa56d8
omni <mohamedelshazly18@yahoo.com>
2022-10-05 00:30:05 +0200

    refactored card details from KanbanItem component to its own

---
+++ src/components/CardDetails.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function CardDetails({item, id}) {
+
+    const closeCardDetails = () => {
+        document.getElementById(`card-details-${id}`).style.display = 'none';
+        document.getElementById(`card-details-${id}`).classList.remove('dim');
+    }
+    
+  return (
+    <div className='card-details' id={`card-details-${id}`}>
+        <div className="card-details-content">
+          <div className="header">
+            <h1>{item.name}</h1>
+            <h3>{item.priority} Priority</h3>
+            <h4>belongs to the <strong>'{item.status}'</strong> status</h4>
+          </div>
+          <div className="description">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias aperiam aliquam error in distinctio, animi consectetur veritatis minima eaque voluptates.</p>
+          </div>
+          <div className="btns">
+            <button className='close' onClick={closeCardDetails}>Close</button>
+          </div>
+        </div>
+      </div>
+  )
+}
--- src/components/KanbanItem.js
+++ src/components/KanbanItem.js
@@ -1,20 +1,32 @@
-import React from 'react'; 
+import React from 'react';
 import { useDrag } from "react-dnd";
+import CardDetails from './CardDetails';
 
 import '../styles/card.css'
 
 export default function KanbanItem({ item }) {
-    const id = item.id;
-    const [{ isDragging }, drag] = useDrag({
-        type: `${item.status}`,
-        item: {id},
-        collect: monitor => ({
-            isDragging: monitor.isDragging()
-        })
-    });
-    const opacity = isDragging ? 0 : 1; 
+  const id = item.id;
+  const [{ isDragging }, drag] = useDrag({
+    type: `${item.status}`,
+    item: { id },
+    collect: monitor => ({
+      isDragging: monitor.isDragging()
+    })
+  });
+  const opacity = isDragging ? 0 : 1;
+
+  const viewDetails = () => {
+    document.getElementById(`card-details-${id}`).style.display = 'block';
+    document.getElementById(`card-details-${id}`).classList.add('dim');
+  }
 
   return (
-    <div ref={drag} className='card' style={{opacity}}>{item.name}--{item.priority}</div>
+    <div>
+      <div ref={drag} className='card' style={{ opacity }} onClick={viewDetails}>
+        {item.name}
+      </div>
+      <CardDetails item={item} id={id} />
+    </div>
+
   )
 }
